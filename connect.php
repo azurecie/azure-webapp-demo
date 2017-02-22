3ecf90db089f23273daa250bc2f9df1800cd46d9
azurecie <adityajanata@outlook.com>
2017-02-22 15:58:25 +0700

    update username

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "dodol_db";
+$host = "ap-cdbr-azure-southeast-b.cloudapp.net";
+$user = "bae0bd10eec6eb";
+$pass = "94552f79";
+$name = "samsungwebappdb";
  
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
